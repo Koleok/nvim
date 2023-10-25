b979aedb3f060711a578a01497e08880bc526537
koleok <kylechamberlain@proton.me>
2023-10-25 11:46:59 -0400

    Disable cutlass because I can't get it to work

--- lua/plugins/cutlass.lua
+++ lua/plugins/cutlass.lua
@@ -1,21 +1,20 @@
 return {
-  -- {
-  --   "gbprod/cutlass.nvim",
-  --   keys = {
-  --     "x",
-  --     "d",
-  --     "c",
-  --     "X",
-  --     "D",
-  --     "C",
-  --     "m",
-  --   },
-  --   lazy = false,
-  --   config = function()
-  --     require("cutlass").setup({
-  --       exclude = { "ns", "nS" },
-  --       cut_key = "<leader>x",
-  --     })
-  --   end,
+  -- "gbprod/cutlass.nvim",
+  -- enabled = false,
+  -- keys = {
+  --   "x",
+  --   "d",
+  --   "c",
+  --   "X",
+  --   "D",
+  --   "C",
+  --   "m",
   -- },
+  -- lazy = false,
+  -- config = function()
+  --   require("cutlass").setup({
+  --     exclude = { "ns", "nS" },
+  --     cut_key = "<leader>x",
+  --   })
+  -- end,
 }
