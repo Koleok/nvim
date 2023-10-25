d1788873d6f2851d89865c431ae51c79c099f965
koleok <kylechamberlain@proton.me>
2023-10-25 11:45:38 -0400

    Add some commented bindings to telescope config in case I want to change them

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,4 +7,10 @@ return {
       require("telescope").load_extension("fzf")
     end,
   },
+  -- keys = {
+  --   { "<leader><space>", require("telescope")("files", { cwd = false }), desc = "Find Files (root dir)" },
+  --   { "<leader>ff", require("telescope")("files", { cwd = false }), desc = "Find Files (root dir)" },
+  --   { "<leader>sg", require("telescope")("live_grep", { cwd = false }), desc = "Grep (root dir)" },
+  --   { "<leader>sw", require("telescope")("grep_string", { cwd = false }), desc = "Word (root dir)" },
+  -- },
 }
